dd729c79735487c10f2e997b57e095d1407902d4
Brandon Villa <bornofos@gmail.com>
2017-10-17 15:21:13 -0500

    Platillo route and model finished

--- models/platillo.js
+++ models/platillo.js
@@ -5,9 +5,9 @@ const Platillo = {};
 Platillo.all = next => {
     if ( !connection )
         return next('Connection refused');
-        connection.query('SELECT * FROM platillo', (error, result) => {
+    connection.query('SELECT * FROM platillo', (error, result) => {
         if ( error )
-            return next({ success: false, error: error });
+            return next({ success: false, error: error })
         else
             return next( null, { success: true, result: result });
     });
@@ -16,29 +16,64 @@ Platillo.all = next => {
 Platillo.findById = (platilloId, next) => {
     if ( !connection )
         return next('Connection refused');
-        connection.query('SELECT * FROM platillo WHERE idplatillo = ?', [platilloId], (error, result) => {
+    connection.query('SELECT * FROM platillo WHERE idplatillo = ?', 
+    [platilloId], (error, result) => {
         if ( error )
-            return next({ success: false, error: error });
+            return next({ success: false, error: error })
         else
-            return next( null, { success: true, result: result });
+            return next( null, { success: true, result: result[0] });
     });
+};
+
+Platillo.count = next => {
+    if ( !connection )
+        return next('Connection refused');
+    connection.query(`SELECT COUNT(idplatillo) AS count FROM platillo`, (error, result) => {
+        if ( error )
+            return next({ success: false, error: error })
+        else
+            return next( null, { success: true, result: result[0] });
+    });
+};
+
+Platillo.exist = (platilloId, next) => {
+    if ( !connection )
+        return next('Connection refused');
+    connection.query('SELECT EXISTS(SELECT 1 FROM platillo WHERE idplatillo = ?) AS exist', [platilloId], (error, result) => {
+        if ( error )
+            return next({ success: false, error: error })
+        else
+            return next( null, { success: true, result: result[0] });
+
+    })
 };
 
 Platillo.insert = (platillo, next) => {
     if ( !connection )
         return next('Connection refused');
-        connection.query(`INSERT INTO platillo SET ?`, [platillo], (error, result, fileds) => {
-        if ( error )
-            return next({ success: false, error: error });
-        else
+    connection.query(`INSERT INTO platillo SET ?`, [platillo], (error, result) => {
+        if ( error ) 
+            return next({ success: false, error: error })
+        else 
             return next( null, { success: true, result: result });
     });
 };
 
-Platillo.response = (res, data, error) => {
+Platillo.update = (platillo, next) => {
+    if ( !connection )
+        return next('Connection refused');
+    connection.query('UPDATE platillo SET ? WHERE idplatillo = ?', [platillo, platillo.idplatillo], (error, result) => {
+        if ( error )
+            return next({ success: false, error: error });
+        else
+            return next( null, { success: true, result: result});
+    });
+};
+
+Platillo.response = (res, error, data) => {
     if ( error )
         res.status(500).json(error);
-    else
+    else 
         res.status(200).json(data);
 };
 
--- routes/platillos.js
+++ routes/platillos.js
@@ -7,18 +7,37 @@ router
             return Platillo.response(res, error, data);
         });
     })
-
+    .get('/count', (req, res, next) => {
+        Platillo.count( (error, data) => {
+            return Platillo.response(res, error, data);
+        });
+    })
+    .get('/exist/:id', (req, res, next) => {
+        Platillo.exist( req.params.id, (error, data) => {
+            return Platillo.response(res, error, data);
+        });
+    })
     .get('/:id', (req, res, next) => {
         const platilloId = req.params.id;
         Platillo.findById( platilloId, (error, data) => {
             return Platillo.response(res, error, data);
         });
     })
-
+    .patch('/', (req, res, next) => {
+        const platillo = {
+            idplatillo: req.body.idplatillo,
+            nombre: req.body.nombre,
+            descripcion: req.body.descripcion,
+            tipoComida_idtipoComida: req.body.tipoComida_idtipoComida,
+            baja: false
+        };
+        Platillo.update( platillo, (error, data) => {
+            return Platillo.response(res, error, data);
+        })
+    })
     .post('/', (req, res, next) => {
-        console.log(req);
-        const platillo =
-        {
+        const platillo = {
+            idplatillo: null,
             nombre: req.body.nombre,
             descripcion: req.body.descripcion,
             tipoComida_idtipoComida: req.body.tipoComida_idtipoComida,
