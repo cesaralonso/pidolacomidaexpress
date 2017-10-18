64e3be7f22ebcede5a134699b1fb3b9fe83f29e2
Brandon Villa <bornofos@gmail.com>
2017-10-18 14:32:20 -0500

    routes imported

--- app.js
+++ app.js
@@ -8,14 +8,22 @@ const passport = require('passport');
 //Route importation.
 const rol = require('./routes/roles');
 const user = require('./routes/users');
+const pago = require('./routes/pagos');
 const combo = require('./routes/combos');
+const orden = require('./routes/ordenes');
+const oferta = require('./routes/ofertas');
 const estado = require('./routes/estados');
 const ciudad = require('./routes/ciudades');
+const horario = require('./routes/horarios');
+const factura = require('./routes/facturas');
 const telefono = require('./routes/telefonos');
 const platillo = require('./routes/platillos');
 const direccion = require('./routes/direcciones');
+const tipoComida = require('./routes/tipoComidas');
 const enfermedad = require('./routes/enfermedades');
+const ingrediente = require('./routes/ingredientes');
 const restaurante = require('./routes/restaurantes');
+const platilloOrdenado = require('./routes/platillosOrdenados');
 
 // Express Instance
 const app = express();
@@ -34,14 +42,22 @@ require('./config/passport')(passport);
 // Warehouses
 app.use('/rol', rol);
 app.use('/user', user);
+app.use('/pago', pago);
+app.use('/orden', orden);
 app.use('/combo', combo);
+app.use('/oferta', oferta);
 app.use('/estado', estado);
 app.use('/ciudad', ciudad);
+app.use('/horario', horario);
+app.use('/factura', factura);
 app.use('/telefono', telefono);
 app.use('/platillo', platillo);
 app.use('/direccion', direccion);
 app.use('/enfermedad', enfermedad);
+app.use('/tipoComida', tipoComida);
+app.use('/ingrediente', ingrediente);
 app.use('/restaurante', restaurante);
+app.use('/platilloOrdenado', platilloOrdenado);
 
 // Set port
 app.listen(3000);
