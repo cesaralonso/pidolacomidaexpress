80c418e3f2b700ecb1c75afbbd753cceb2f077e7
Brandon Villa <bornofos@gmail.com>
2017-10-26 14:40:06 -0500

    Dynamic query that allows to add a relation into a collection

--- services/dynamic-queries.js
+++ services/dynamic-queries.js
@@ -2,27 +2,35 @@ const async = require('async');
 const connection = require('../config/db-connection');
 
 const DynamicQuery = { };
-
-DynamicQuery.getElementsByColumn = (table, column, value, pivotTable, next) => {
+/**
+ * @param items => The collection that it will have a new relation included for each of its elements
+ * @param tableToRelate => Table to get the relation
+ * @param itemColumnToRelate => Column of @items to relate
+ * @param tableToRelateColumn => Column of @tableToRelate to relate
+ * @param next => Callback
+ * 
+ * Given a table set a relation to a collection
+ */
+DynamicQuery.addRelation = (items, tableToRelate, itemColumnToRelate, tableToRelateColumn, next) => {
     if ( !connection )
         return next('Connection refused');
-    async.waterfall([
-        next => {
-            connection.query(`SELECT * FROM ?? WHERE ?? = ?`, 
-            [table, column, value], (error, result) => {
-                if ( error )
-                    return next(error)
-                else
-                    return next(null, result)
-            })
-        },
-        (items, next) => {
-            async.each(items, (item, nextIteration) => {
-                console.log(item);
-                nextIteration();
-            })
-        }
-    ])
+    async.each( items, (item, nextIteration) => {
+        connection.query(`SELECT * FROM ?? WHERE ?? = ?`,
+        [tableToRelate, tableToRelateColumn, item[itemColumnToRelate]], (error, result) => {
+            if ( error )
+                return next(error);
+            else {
+                item[tableToRelate] = result[0];
+                return nextIteration();
+            }
+        })
+    },
+    err => {
+        if (err)
+            return next(err);
+        else
+            return next(null, items)
+    })
 }
 
 module.exports = DynamicQuery;
