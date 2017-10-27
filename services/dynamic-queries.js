5c9573438ed367a80ab635cbc5b285fc26d3178a
Brandon Villa <bornofos@gmail.com>
2017-10-26 19:38:05 -0500

    Now the new relation can be named

--- services/dynamic-queries.js
+++ services/dynamic-queries.js
@@ -5,22 +5,22 @@ const DynamicQuery = { };
 /**
  * @param collection => The collection that it will have a new relation included for each of its elements
  * @param tableToRelate => Table to get the relation
- * @param itemColumnToRelate => Column of @items to relate
+ * @param collectionColumnToRelate => Column of @collections to relate
  * @param tableToRelateColumn => Column of @tableToRelate to relate
  * @param next => Callback
  * 
  * Given a table, set a relation to a collection
  */
-DynamicQuery.addRelation = (collection, tableToRelate, itemColumnToRelate, tableToRelateColumn, next) => {
+DynamicQuery.addRelation = (collection, tableToRelate, collectionColumnToRelate, tableToRelateColumn, newRelationName, next) => {
     if ( !connection )
         return next('Connection refused');
     async.each( collection, (item, nextIteration) => {
         connection.query(`SELECT * FROM ?? WHERE ?? = ?`,
-        [tableToRelate, tableToRelateColumn, item[itemColumnToRelate]], (error, result) => {
+        [tableToRelate, tableToRelateColumn, item[collectionColumnToRelate]], (error, result) => {
             if ( error )
                 return next(error);
             else {
-                item[tableToRelate] = result[0];
+                item[newRelationName] = result[0];
                 return nextIteration();
             }
         })
